2c755a8f25f58452703e9eadd266085b802bf308
kathLeenShawz <kathleenshaw1578@iejesusmirey.com>
2025-11-13 08:55:27 -0600

    feat: add src/extractors/utils.py

---
+++ src/extractors/utils.py
@@ -0,0 +1,10 @@
+thonimport logging
+
+def setup_logger():
+    logger = logging.getLogger('InfoJobsScraper')
+    handler = logging.StreamHandler()
+    formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
+    logger.setLevel(logging.INFO)
+    return logger
